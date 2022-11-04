ba5e640a7ab6a19c62bb823cdda1f53dd01f85ac
Victor Medina <vmedina.qcode.ext@luegopago.com>
2022-11-03 19:09:18 -0500

    update doc

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# eventos_javascript
+# Web event manager
+
+Web para administrar eventos en una institución. utilizando HTML, CSS, Bootstrap Y Javascript.
+
+## Implementation
+
+- instale la extension en tu VSCode `Live Server` para simular un servidor
+- cambie la url del JSON con la lista de las canciones en scriptC.js y scriptl.js si asi lo requiere puerto por defecto `http://127.0.0.1:5500/datos.json`
+- abrir en index.html presionar clic derecho en el mismo y escoger la opcion `Open with live server` automaticamente correra la aplicacion en tu navegador predeterminado.
+
+## Note
+
+- aplicacion usando jquery, bootstrap, javascript, html, css. Permite cargar en la vista los eventos para determinada institución, en el proyecto raiz se encuentra un archivo `info.json` que simula las respuestas del api con la informacion a renderizar.
--- js/pasados.js
+++ js/pasados.js
@@ -4,35 +4,32 @@ var eventos;
 var pasados = [];
 
 $(document).ready(function () {
-
   //Carga los datos que estan en el JSON (info.json) usando AJAX
   $.ajax({
-    url: "http://127.0.0.1:5500/info.json"
+    url: "http://127.0.0.1:5500/info.json",
   }).done(function (eventosOb) {
-
-  //Guarda el resultado en variables
-  hoy = eventosOb.fechaActual;
-eventos = eventosOb.eventos;
-  //Selecciona los eventos que sean anteriores a la fecha actual del JSON
-  for (var i = 0; i < eventos.length; i++) {
-
-    if (hoy > eventos[i].fecha) {
-      //proximos
-      pasados.push(eventos[i]);
+    //Guarda el resultado en variables
+    hoy = eventosOb.fechaActual;
+    eventos = eventosOb.eventos;
+    //Selecciona los eventos que sean anteriores a la fecha actual del JSON
+    for (var i = 0; i < eventos.length; i++) {
+      if (hoy > eventos[i].fecha) {
+        //proximos
+        pasados.push(eventos[i]);
+      }
     }
-  }
-  //Ordena los eventos segun la fecha (los mas recientes primero)
-  pasados = pasados.sort(function (x, y) {
-    if (x.fecha < y.fecha) {
-      return 1;
-    }
-    return -1;
-  });
-  //Crea un string que contenga el HTML que describe el detalle del evento
-  var html = "";
-  //Recorre el arreglo y concatena el HTML para cada evento
-  for (var j = 0; j < pasados.length; j++) {
-    html += ` 
+    //Ordena los eventos segun la fecha (los mas recientes primero)
+    pasados = pasados.sort(function (x, y) {
+      if (x.fecha < y.fecha) {
+        return 1;
+      }
+      return -1;
+    });
+    //Crea un string que contenga el HTML que describe el detalle del evento
+    var html = "";
+    //Recorre el arreglo y concatena el HTML para cada evento
+    for (var j = 0; j < pasados.length; j++) {
+      html += ` 
     <div class="rounded col-12 bg-light px-4 py-2 my-3">
     <a class="text-primary" href="detalle.html?id=${pasados[j].id}">
     <h2>${pasados[j].nombre}</h2>
@@ -41,9 +38,9 @@ eventos = eventosOb.eventos;
     <p>${pasados[j].descripcion}</p>
     <p class="text-info">Costo : ${pasados[j].precio}</p>
     </div>
-    `
-  }
-  //Modifica el DOM agregando el html generado
-  document.getElementById("pasados").innerHTML = html
-});
+    `;
+    }
+    //Modifica el DOM agregando el html generado
+    document.getElementById("pasados").innerHTML = html;
+  });
 });
